113a0e5556f57ed71844e47734ac70b0e772a865
AyouBuinany <Ayoub.elbouinany99@gmail.com>
2020-09-04 12:13:43 +0100

    first commit

--- inc/head-tags.php
+++ inc/head-tags.php
@@ -1,4 +1,5 @@
 <meta charset="utf-8">
-<link href="fontawesome/css/all.css" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="fontawesome/css/all.css" rel="stylesheet">
 <link rel="stylesheet" href="css/style.css">
+<link rel="icon" href="images/logo.png">
